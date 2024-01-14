c42711461d7669adb1a3f640b2205ebad13c9727
Krish <Krishkumer150@gamil.com>
2024-01-14 16:34:06 +0530

    push-react-code

---
+++ src/Header.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Header = (Props) => {
+  return <h1>{Props.title}</h1>;
+};
+
+export default Header;
---
+++ src/index.js
@@ -0,0 +1,14 @@
+import ReactDOM from "react-dom/client";
+import Header from "./Header";
+import React from "react";
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <Header title="Benkai" />
+  </React.StrictMode>
+);
+
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
