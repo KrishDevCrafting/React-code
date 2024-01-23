56980faa5931e547d1e328603f97c1a28e75a6ae
Krish <Krishkumer150@gamil.com>
2024-01-23 11:17:38 +0530

    useState

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import ReactDOM from "react-dom/client";
 import React from "react";
 import First from "./First";
+import Value from "./Dynamic";
+
+import InputComponent from "./Pears";
+// import App from "./Guss";
+// import PasswordGenerator from "./PasswordGenerater";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <First />
+    <Value />
+    <InputComponent />
   </React.StrictMode>
 );
