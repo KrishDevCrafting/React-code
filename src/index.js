0342c0755223569421cd7faec9864883928f79ac
Krish <Krishkumer150@gamil.com>
2024-01-25 15:20:30 +0530

    react

--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@ import ReactDOM from "react-dom/client";
 import React from "react";
 import First from "./First";
 import Value from "./Dynamic";
-
+import Data from "./Data";
 import InputComponent from "./Pears";
 // import App from "./Guss";
 // import PasswordGenerator from "./PasswordGenerater";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <Data name="krish" />
+    <Data />
+    <Data />
     <First />
     <Value />
     <InputComponent />
