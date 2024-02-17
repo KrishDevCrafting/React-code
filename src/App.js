09ff57fad9755b45d6e3a46af557c1a527a09695
krish <youremail@example.com>
2024-02-17 11:42:20 +0530

    Update-React....~

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import Homepage from "./Home";
 import AboutMe from "./aboutMe";
+<<<<<<< HEAD
 import { Routes , Route } from "react-router-dom";}
+=======
+import { Routes, Route, Link } from "react-router-dom";
+>>>>>>> 574e3f0 (admin component added)
 function App() {
   return (
     <div className="App">
       <nav className="navbar navbar-light bg-light">
+<<<<<<< HEAD
         <a href="#" className="navbar-brand">
           Homepage
         </a>
@@ -15,6 +20,14 @@ function App() {
       <Routes>
       <Route path ="/" element ={<Homepage/>} />
       <Route path ="/" element ={<AboutMe/>} />
+=======
+        <Link path="/">Homepage</Link>
+        <Link path="about-me">About-Me</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="about-me" element={<AboutMe />} />
+>>>>>>> 574e3f0 (admin component added)
       </Routes>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -4,14 +4,27 @@ import First from "./First";
 import Todo from "./Tanay";
 import Example1 from "./index.jsx";
 import App from "./App.js";
+<<<<<<< HEAD
 import { BrowserRouter } from "react-router-dom";}
+=======
+import Info from "./InterSteller.jsx";
+import { BrowserRouter } from "react-router-dom";
+>>>>>>> 574e3f0 (admin component added)
 const main = ReactDOM.createRoot(document.getElementById("main"));
 
 main.render(
   <React.StrictMode>
+<<<<<<< HEAD
     <div><BrowserRouter>
       <App />
       </BrowserRouter>
+=======
+    <div>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+      <Info />
+>>>>>>> 574e3f0 (admin component added)
       <First />
       <Todo />
       <Example1 />
