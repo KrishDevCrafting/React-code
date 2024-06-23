8db6329109392debb10109227b28cbb76de91c88
KrishDevCrafting <krishkumer150@gmail.com>
2024-06-24 00:04:55 +0530

    Updated

---
+++ src/home/TextAnimation.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import "../home/text.css"
+const TextComponet = ()=>{
+return<>
+
+
+<div>
+<div id='text'><h1>Escape</h1></div> 
+<span id='text'><h1>into amazing experiences</h1></span>
+<div > 
+  
+</div>
+
+
+<p>a css3 animation demo</p>
+</div>
+
+</>
+}
+
+
+export default TextComponet;
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import First from "./First";
 import Todo from "./Tanay";
 import Example1 from "./index.jsx";
 import App from "./App.js";
-
+import TextComponet from "./home/TextAnimation.jsx";
 
 import Info from "./InterSteller.jsx";
 import { BrowserRouter } from "react-router-dom";
@@ -15,6 +15,7 @@ const main = ReactDOM.createRoot(document.getElementById("main"));
 
 main.render(
   <React.StrictMode>
+    <TextComponet/>
 <TakeData/>
     <Info/>
   </React.StrictMode>
